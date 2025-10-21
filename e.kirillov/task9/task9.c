fe2f483978cd7a7e4c6b91d4d4fc2b73090d20c8
Eugen <kirillov2255@yandex.ru>
2025-10-21 13:18:59 +0700

    add task9

---
+++ e.kirillov/task9/task9.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // Дочерний процесс
+        execlp("cat", "childProccess", "../task3/task3.c", NULL);
+        // Если сюда попали - значит ошибка
+        perror("execlp failed");
+        return 1;
+    }
+    else {
+        // Родительский процесс
+        if(wait(NULL) != -1) {
+            printf("\nChild process (pid: %d) finished\n", pid);
+        }
+    }
+
+    return 0;
+}
