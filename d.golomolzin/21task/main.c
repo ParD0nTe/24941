1bbdf344fb897624ef56253142f0d72df7b2b1b1
Daniil Golomolzin <golomolzindaniil321@gmail.com>
2025-10-28 13:41:18 +0700

    add [21] for test

---
+++ d.golomolzin/21task/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+int count = 0;
+
+// будет писщать при нажатии CTRL^C
+void signal_SIGINT() {
+
+    // буквально звук
+    printf("\a");
+
+    fflush(NULL);
+    count++;
+}
+
+// будет завершать работу цикла
+void signal_SIGQUIT() {
+    
+    printf("\nСигнал прозвучал %d раз\n", count);
+    exit(0);
+}
+
+
+int main() {
+
+    signal(SIGINT,  &signal_SIGINT);
+    signal(SIGQUIT, &signal_SIGQUIT);
+
+    for(;;);
+}
