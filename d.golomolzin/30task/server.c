5a30eaed700bbcf743cddde1e9e1f208e96543b8
Daniil Golomolzin <golomolzindaniil321@gmail.com>
2025-11-10 12:38:10 +0700

    Update [30] for Testing

--- d.golomolzin/30task/server.c
+++ d.golomolzin/30task/server.c
@@ -37,7 +37,7 @@ int main () {
     
     // хранится же локально, значит можно указать свой путь
     // const char* path_socket = "/mnt/d/Desktop/VSCODE/24941/d.golomolzin/30task/socket";
-    const char* path_socket = "/tmp/socket";
+    const char* path_socket = "/home/students/24200/d.golomolzin/24941/d.golomolzin/30task/socket";
     // если неполучилось создать И если не ошибка отсутсвия файла (ПРОСТО ОТЧИСТКА ПРЕДЫДУЩЕГО СОКЕТА)
     if (unlink(path_socket) == -1 && errno != 2) {
         perror("unlink");
