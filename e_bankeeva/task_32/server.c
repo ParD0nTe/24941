b35964ab01051db87f1cdae7a8413084b4273b58
evabankeeva <bankeeva.eva@yandex.ru>
2025-12-09 00:02:57 +0700

    task_31-32

--- e_bankeeva/task_32/server.c
+++ e_bankeeva/task_32/server.c
@@ -16,24 +16,22 @@
 int server_fd;
 int client_fd[5];
 int client_num[5];
-int next_client_id = 1;
+int next_id = 1;
 
-void timestamp(char *buf, size_t sz) {
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
+struct timespec start_ts, end_ts;
 
-    struct tm tm_info;
-    localtime_r(&ts.tv_sec, &tm_info);
+void print_runtime() {
+    clock_gettime(CLOCK_MONOTONIC, &end_ts);
+    double sec = (end_ts.tv_sec - start_ts.tv_sec)
+               + (end_ts.tv_nsec - start_ts.tv_nsec) / 1e9;
 
-    strftime(buf, sz, "%Y-%m-%d %H:%M:%S", &tm_info);
-    sprintf(buf + strlen(buf), ".%03ld", ts.tv_nsec / 1000000);
+    printf("\n=== SERVER RUNTIME: %.3f sec ===\n", sec);
 }
 
 void handle_sigpoll(int sig)
 {
     (void)sig;
 
-    char ts[64], out[256];
     int fd;
 
     while ((fd = accept(server_fd, NULL, NULL)) >= 0) {
@@ -44,10 +42,9 @@ void handle_sigpoll(int sig)
         for (int i = 0; i < 5; i++) {
             if (client_fd[i] == -1) {
                 client_fd[i] = fd;
-                client_num[i] = next_client_id++;
+                client_num[i] = next_id++;
 
-                timestamp(ts, sizeof(ts));
-                printf("client_%d connected %s\n", client_num[i], ts);
+                printf("client_%d connected\n", client_num[i]);
                 fflush(stdout);
                 break;
             }
@@ -55,28 +52,32 @@ void handle_sigpoll(int sig)
     }
 
     for (int i = 0; i < 5; i++) {
-
-        fd = client_fd[i];
-        if (fd < 0) continue;
+        int cfd = client_fd[i];
+        if (cfd < 0) continue;
 
         char c;
-        ssize_t r = read(fd, &c, 1);
+        ssize_t r = read(cfd, &c, 1);
 
         if (r > 0) {
             c = toupper((unsigned char)c);
-
-            timestamp(ts, sizeof(ts));
-            printf("client_%d: %s %c\n", client_num[i], ts, c);
+            printf("client_%d %c\n", client_num[i], c);
             fflush(stdout);
         }
         else if (r == 0 || (r < 0 && errno != EAGAIN)) {
 
-            timestamp(ts, sizeof(ts));
-            printf("client_%d disconnected %s\n", client_num[i], ts);
+            printf("client_%d disconnected\n", client_num[i]);
 
-            close(fd);
+            close(cfd);
             client_fd[i] = -1;
-            client_num[i] = 0;
+
+            int active = 0;
+            for (int k = 0; k < 5; k++)
+                if (client_fd[k] != -1) active = 1;
+
+            if (!active) {
+                print_runtime();
+                exit(0);
+            }
         }
     }
 }
@@ -85,19 +86,19 @@ int main()
 {
     struct sockaddr_un addr;
 
+    clock_gettime(CLOCK_MONOTONIC, &start_ts);
+
     for (int i = 0; i < 5; i++)
         client_fd[i] = -1;
 
     server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket"); return 1;
-    }
+    if (server_fd < 0) { perror("socket"); return 1; }
 
     unlink(SOCKET_PATH);
 
     memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path)-1);
 
     if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
         perror("bind"); return 1;
@@ -107,20 +108,14 @@ int main()
         perror("listen"); return 1;
     }
 
-    printf("wait for client...");
+    printf("wait for clients...\n");
 
     fcntl(server_fd, F_SETFL, O_NONBLOCK);
     ioctl(server_fd, I_SETSIG, S_INPUT | S_HANGUP | S_ERROR);
 
-    struct sigaction sa;
+    struct sigaction sa = {0};
     sa.sa_handler = handle_sigpoll;
-    sa.sa_flags = 0;
-    sigemptyset(&sa.sa_mask);
     sigaction(SIGPOLL, &sa, NULL);
 
-    printf("WAIT FOR CLIENTS...\n");
-
     while (1) pause();
-
-    return 0;
 }
