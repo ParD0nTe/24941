eb38b9a975ae2be2ed5fbb30a34f9180b7952fc8
evabankeeva <bankeeva.eva@yandex.ru>
2025-12-09 12:44:28 +0700

    task_31

--- e_bankeeva/task_32/client.c
+++ e_bankeeva/task_32/client.c
@@ -16,25 +16,35 @@ int main(int argc, char* argv[]) {
     }
 
     int num = atoi(argv[1]);
-    const char *msg = (num == 1 ? "hello" : "goodbye");
+
+    const char *msg;
+    if (num == 1)
+        msg = "hello";
+    else
+        msg = "goodbye";
 
     int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd == -1) { perror("socket"); return 1; }
 
-    struct sockaddr_un address = {0};
+    struct sockaddr_un address;
+    memset(&address, 0, sizeof(address));
     address.sun_family = AF_UNIX;
-    strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path)-1);
+    strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) - 1);
 
-    connect(fd, (struct sockaddr*)&address, sizeof(address));
-
-    int len = strlen(msg);
-
-    for (int repeat = 0; repeat < 5; repeat++) {
-        for (int i = 0; i < len; i++) {
-            char c = msg[i];
-            write(fd, &c, 1);
-            usleep(100000);
-        }
+    if (connect(fd, (struct sockaddr*)&address, sizeof(address)) == -1) {
+        perror("connect");
+        close(fd);
+        return 1;
     }
+
+    char buf[128];
+    snprintf(buf, sizeof(buf), "client_%d: %s\n", num, msg);
+    for (int i = 0; i < 5; i++) {
+        snprintf(buf, sizeof(buf), "client_%d: %s\n", num, msg);
+        write(fd, buf, strlen(buf));
+        usleep(100000);
+    }
+    close(fd);
 
     close(fd);
     return 0;
