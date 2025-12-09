a96265926e3172e5b8d00b57e7b812dcf0e7a28c
Anton Bykov <a.bykov4@g.nsu.ru>
2025-12-09 12:00:44 +0700

    change 30

--- a.bykov4/30_task/client.c
+++ a.bykov4/30_task/client.c
@@ -1,26 +1,49 @@
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define SOCK_PATH "./socket"
+#define SOCKET_PATH "./socket"
 
-int main() {
-    int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+// низкая функция записи текста в сокет (без времени и задержки)
+void write_(int fd, int num) {
+    const char* texts[] = {
+        "first", "second", "third", "fourth", "fifth",
+        "sixth", "seventh", "eighth", "ninth", "tenth"
+    };
 
-    struct sockaddr_un addr = { .sun_family = AF_UNIX };
-    strncpy(addr.sun_path, SOCK_PATH, sizeof(addr.sun_path)-1);
+    char buf[64];
+    for (int i = 0; i < 10; i++) {
+        const char* msg = texts[rand() % 10];
+        snprintf(buf, sizeof(buf), "[%d] %s\n", num, msg);
+        write(fd, buf, strlen(buf));
+    }
+}
 
-    if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-        perror("connect");
-        return 1;
+int main(int argc, char* argv[]) {
+    srand(time(NULL));
+
+    int fd;
+    struct sockaddr_un address;
+
+    fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd == -1) { perror("socket"); return 1; }
+
+    memset(&address, 0, sizeof(address));
+    address.sun_family = AF_UNIX;
+    strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) - 1);
+
+    if (connect(fd, (struct sockaddr*)&address, sizeof(address)) == -1) {
+        perror("connect"); close(fd); return 1;
     }
 
-    const char *msg = "this is client messege\n";
-    write(fd, msg, strlen(msg));
+    int num = argc > 1 ? atoi(argv[1]) : 1;
+    write_(fd, num);
 
     close(fd);
     return 0;
 }
-
--- a.bykov4/30_task/server.c
+++ a.bykov4/30_task/server.c
@@ -1,35 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
 
-#define SOCK_PATH "./socket"
+#define SOCKET_PATH "./socket"
+#define BUFFER_SIZE 1024
 
-int main() {
-    int srv = socket(AF_UNIX, SOCK_STREAM, 0);
+int main()
+{
+    int server_fd, client_fd;
+    struct sockaddr_un server_addr, client_addr;
+    socklen_t client_len;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
 
-    struct sockaddr_un addr = { .sun_family = AF_UNIX };
-    strncpy(addr.sun_path, SOCK_PATH, sizeof(addr.sun_path)-1);
+    unlink(SOCKET_PATH);
 
-    unlink(SOCK_PATH);
-    bind(srv, (struct sockaddr*)&addr, sizeof(addr));
-    listen(srv, 1);
+    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
 
-    int cli = accept(srv, NULL, NULL);
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
 
-    char buf[256];
-    ssize_t n;
-
-    while ((n = read(cli, buf, sizeof(buf))) > 0) {
-        for (int i = 0; i < n; i++)
-            buf[i] = toupper((unsigned char)buf[i]);
-        write(STDOUT_FILENO, buf, n);
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("bind");
+        close(server_fd);
+        exit(EXIT_FAILURE);
     }
 
-    close(cli);
-    close(srv);
+    printf("Server listening on %s\n", SOCKET_PATH);
+
+    if (listen(server_fd, 1) == -1)
+    {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    client_len = sizeof(client_addr);
+    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+
+    printf("Client connected\n");
+
+    while ((bytes_read = read(client_fd, buffer, BUFFER_SIZE - 1)) > 0)
+    {
+        buffer[bytes_read] = '\0';
+        for (ssize_t i = 0; i < bytes_read; i++)
+            buffer[i] = toupper((unsigned char)buffer[i]);
+
+        printf("Received: %s", buffer);
+        fflush(stdout);
+    }
+
+    printf("Client disconnected\n");
+
+    close(client_fd);
+    close(server_fd);
+    unlink(SOCKET_PATH);
+
     return 0;
 }
-
