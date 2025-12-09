848dade8ca969ed7cc437da2f24a1b8b373ae84d
Anton Bykov <a.bykov4@g.nsu.ru>
2025-12-09 12:54:36 +0700

    change 30

--- a.bykov4/30_task/client.c
+++ a.bykov4/30_task/client.c
@@ -7,43 +7,40 @@
 #include <string.h>
 #include <time.h>
 
-#define SOCKET_PATH "./socket"
-
-// низкая функция записи текста в сокет (без времени и задержки)
-void write_(int fd, int num) {
-    const char* texts[] = {
-        "first", "second", "third", "fourth", "fifth",
-        "sixth", "seventh", "eighth", "ninth", "tenth"
-    };
-
-    char buf[64];
-    for (int i = 0; i < 10; i++) {
-        const char* msg = texts[rand() % 10];
-        snprintf(buf, sizeof(buf), "[%d] %s\n", num, msg);
-        write(fd, buf, strlen(buf));
-    }
-}
+#define S_PATH "./socket"
 
 int main(int argc, char* argv[]) {
     srand(time(NULL));
 
-    int fd;
-    struct sockaddr_un address;
+    const char* months[] = {
+        "January", "February", "March", "April", "May",
+        "June", "July", "August", "September", "October"
+    };
 
-    fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (fd == -1) { perror("socket"); return 1; }
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0) { perror("socket"); return 1; }
 
-    memset(&address, 0, sizeof(address));
-    address.sun_family = AF_UNIX;
-    strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) - 1);
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, S_PATH, sizeof(addr.sun_path) - 1);
 
-    if (connect(fd, (struct sockaddr*)&address, sizeof(address)) == -1) {
-        perror("connect"); close(fd); return 1;
+    if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("connect");
+        close(sock);
+        return 1;
     }
 
-    int num = argc > 1 ? atoi(argv[1]) : 1;
-    write_(fd, num);
+    int tag = (argc > 1) ? atoi(argv[1]) : 1;
 
-    close(fd);
+    char out[64];
+    for (int i = 0; i < 10; i++) {
+        const char* m = months[rand() % 10];
+        snprintf(out, sizeof(out), "[%d] %s\n", tag, m);
+        write(sock, out, strlen(out));
+    }
+
+    close(sock);
     return 0;
 }
+
